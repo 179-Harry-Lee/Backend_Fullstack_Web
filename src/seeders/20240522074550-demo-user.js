f4ca4b9f6724236b5db85be799fedd34c81c95d1
Wesley Dung <ducdung17903@gmail.com>
2024-05-22 14:53:11 +0700

    Connect DB

---
+++ src/seeders/20240522074550-demo-user.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        email: "example@example.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
