ae76d6db0ce2e48bded37d4e3bb60b79885c73cb
Wesley Dung <ducdung17903@gmail.com>
2024-05-31 01:42:08 +0700

    Connect API to frontend

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ import bodyParser from "body-parser"; // query parameter
 import viewEngine from "./config/viewEngine";
 import initWebRouets from "./route/web";
 import connectDB from "./config/connectDB"; // connect;
-
+import cors from "cors";
 require("dotenv").config();
 
 let app = express();
-
+app.use(cors({ credentials: true, origin: true }));
 //config app
 
 app.use(bodyParser.json());
