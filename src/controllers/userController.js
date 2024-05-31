0575d386da0c51c2b6b19f3e0163e6890e40690d
Wesley Dung <ducdung17903@gmail.com>
2024-05-31 22:18:45 +0700

    Create API connect Frontend use to render all data

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,7 @@ let handleLogin = async (req, res) => {
 };
 
 let handleGetAllUsers = async (req, res) => {
-  let id = req.body.id; //ALL, id
+  let id = req.query.id; //ALL, id
 
   if (!id) {
     return res
