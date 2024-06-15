a97e3aefb6c49e7a1379d596559ff3a36cbb932e
Wesley Dung <ducdung17903@gmail.com>
2024-06-15 14:33:52 +0700

    Fix lại migration-create

--- src/services/userService.js
+++ src/services/userService.js
@@ -118,8 +118,9 @@ let createNewUser = (data) => {
           lastName: data.lastName,
           address: data.address,
           phonenumber: data.phonenumber,
-          gender: data.gender === "1" ? true : false,
+          gender: data.gender,
           roleId: data.roleId,
+          positionId: data.positionId,
         });
         resolve({
           errCode: 0,
