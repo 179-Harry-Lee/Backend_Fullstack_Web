d5679cc1b522e7c4f4c9bf09a444cfaa63debbb2
Wesley Dung <ducdung17903@gmail.com>
2024-06-17 21:02:00 +0700

    Fix api update

--- src/services/userService.js
+++ src/services/userService.js
@@ -158,7 +158,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
-      if (!data.id) {
+      if (!data.id || !data.roleId || !data.positionId || !data.gender) {
         resolve({
           errCode: 2,
           errMessage: "Missing required parameter",
@@ -173,6 +173,10 @@ let updateUserData = (data) => {
         user.firstName = data.firstName;
         user.lastName = data.lastName;
         user.address = data.address;
+        user.roleId = data.roleId;
+        user.positionId = data.positionId;
+        user.gender = data.gender;
+        user.phonenumber = data.phonenumber;
         await user.save({
           // firstName: data.firstName,
           // lastName: data.lastName,
