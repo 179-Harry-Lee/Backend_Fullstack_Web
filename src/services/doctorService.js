d47e9399a48eafd6d0a21dcb5fa7d13fbd5c78c1
Wesley Dung <ducdung17903@gmail.com>
2024-06-20 16:16:23 +0700

    Update API

---
+++ src/services/doctorService.js
@@ -0,0 +1,38 @@
+import db from "../models/index";
+
+let getTopDoctorHome = (limitInput) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = await db.User.findAll({
+        limit: limitInput,
+        where: { roleId: "R2" },
+        order: [["createdAt", "DESC"]],
+        attributes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: db.Allcode,
+            as: "positionData",
+            attributes: ["valueEn", "valueVi"],
+          },
+          {
+            model: db.Allcode,
+            as: "genderData",
+            attributes: ["valueEn", "valueVi"],
+          },
+        ],
+        raw: true,
+        nest: true,
+      });
+
+      resolve({ errCode: 0, data: users });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  getTopDoctorHome: getTopDoctorHome,
+};
