0670a63336b98a1da3dd4fb29179a10de5fbbb50
Wesley Dung <ducdung17903@gmail.com>
2024-06-24 23:09:15 +0700

    24/6/2024: Thêm điều kiện ở doctorService

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -56,19 +56,37 @@ let saveDetailInfoDoctor = (inputData) => {
       if (
         !inputData.doctorId ||
         !inputData.contentHTML ||
-        !inputData.contentMarkdown
+        !inputData.contentMarkdown ||
+        !inputData.action
       ) {
         resolve({
           errCode: 1,
           errMessage: "Missing parameter",
         });
       } else {
-        await db.Markdown.create({
-          contentHTML: inputData.contentHTML,
-          contentMarkdown: inputData.contentMarkdown,
-          description: inputData.description,
-          doctorId: inputData.doctorId,
-        });
+        if (inputData.action === "CREATE") {
+          await db.Markdown.create({
+            contentHTML: inputData.contentHTML,
+            contentMarkdown: inputData.contentMarkdown,
+            description: inputData.description,
+            doctorId: inputData.doctorId,
+          });
+        } else if (inputData.action === "EDIT") {
+          let doctorMarkdown = await db.Markdown.findOne({
+            where: {
+              doctorId: inputData.doctorId,
+            },
+            raw: false,
+          });
+          if (doctorMarkdown) {
+            doctorMarkdown.contentHTML = inputData.contentHTML;
+            doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
+            doctorMarkdown.description = inputData.description;
+            doctorMarkdown.updatedAt = new Date();
+            await doctorMarkdown.save();
+          }
+        }
+
         resolve({
           errCode: 0,
           errMessage: "Save infor doctor success",
