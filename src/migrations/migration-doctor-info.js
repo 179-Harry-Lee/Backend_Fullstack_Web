c358a6bec52559f071a865e0ea82086bc9661b23
Wesley Dung <ducdung17903@gmail.com>
2024-07-02 19:12:47 +0700

    7/2/2024- Create table doctor_info

---
+++ src/migrations/migration-doctor-info.js
@@ -0,0 +1,65 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("doctor_Info", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      doctorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      priceId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      provinceId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      paymentId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      addressClinic: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      nameClinic: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      note: {
+        type: Sequelize.STRING,
+      },
+
+      count: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("doctor_Info");
+  },
+};
